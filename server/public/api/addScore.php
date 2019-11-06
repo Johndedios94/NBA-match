e0296289b3a5dd9af12eb1880f336b914fb790a0
johndedios94 <johndedios94@gmail.com>
2019-11-06 22:25:42 +0000

    removed var dump

--- server/public/api/addScore.php
+++ server/public/api/addScore.php
@@ -10,23 +10,12 @@ $bodyData = getBodyData();
 $name = $bodyData["name"];
 $score = $bodyData["score"];
 
-// var_dump("name is  ", $name );
-// var_dump("score is  ", $score);
+
 
 $query="INSERT INTO `highScores` ( `name`, `score`) VALUES ( '$name', $score)";
-// var_dump(" query is", $query);
-// $query="SELECT * FROM `highScores`";
-// $result =  mysqli_query($conn, $query);
 
 $result = mysqli_query($conn, $query);
-var_dump("Result is ", $result);
-// $data = [];
-// while($row = mysqli_fetch_assoc($result)){
-//   $data[]=$row;
-// }
 
-// $output = json_encode($data);
-// print($output);
 
 
 ?>
