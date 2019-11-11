af7ab6ee60fc5e74137c5d04333c8a938fa72ee0
johndedios94 <johndedios94@gmail.com>
2019-11-11 01:46:48 +0000

    cleaned up code

--- server/public/api/addScore.php
+++ server/public/api/addScore.php
@@ -6,16 +6,11 @@ require_once('dbconnection.php');
 startup();
 
 $bodyData = getBodyData();
-
 $name = $bodyData["name"];
 $score = $bodyData["score"];
-
-
 
 $query="INSERT INTO `highScores` ( `name`, `score`) VALUES ( '$name', $score)";
 
 $result = mysqli_query($conn, $query);
-
-
 
 ?>
